fec61484517fa95d27dd536da71821115eab9fd9
namoshika <namoshika@users.noreply.github.com>
2014-12-09 17:26:55 +0900

    NuPack時にビルドも一緒に行うようにした
    古いバージョンをパックするより、時間が掛かってもビルドをしてしまったほうがまし。

--- NuPacker.bat
+++ NuPacker.bat
@@ -1,4 +1,4 @@
 SET NuGet=".\packages\NuGet.CommandLine.2.8.3\tools\NuGet.exe"
-%NuGet% pack "SnkLib.App.CookieGetter\SnkLib.App.CookieGetter.csproj" -OutputDirectory ".\Publish" -Properties Configuration=Release -IncludeReferencedProjects
-%NuGet% pack "SnkLib.App.CookieGetter.Forms\SnkLib.App.CookieGetter.Forms.csproj" -OutputDirectory ".\Publish" -Properties Configuration=Release -IncludeReferencedProjects
-%NuGet% pack "CookieGetterSharp\CookieGetterSharp.csproj" -OutputDirectory ".\Publish" -Properties Configuration=Release -IncludeReferencedProjects
+%NuGet% pack "SnkLib.App.CookieGetter\SnkLib.App.CookieGetter.csproj" -OutputDirectory ".\Publish" -Build -Properties Configuration=Release -IncludeReferencedProjects
+%NuGet% pack "SnkLib.App.CookieGetter.Forms\SnkLib.App.CookieGetter.Forms.csproj" -OutputDirectory ".\Publish" -Build -Properties Configuration=Release -IncludeReferencedProjects
+%NuGet% pack "CookieGetterSharp\CookieGetterSharp.csproj" -OutputDirectory ".\Publish" -Build -Properties Configuration=Release -IncludeReferencedProjects
