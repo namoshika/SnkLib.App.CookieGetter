21330eb137ed22f555c40df1b310ebc31d61197a
namoshika <namoshika@users.noreply.github.com>
2015-05-09 19:36:14 +0900

    参照ライブラリを最新版へ更新した
    Nuspecに記述している依存ライブラリのバージョンも更新。

--- Nuspecs/NuPacker.bat
+++ Nuspecs/NuPacker.bat
@@ -1,3 +1,3 @@
-SET NuGet="..\packages\NuGet.CommandLine.2.8.3\tools\NuGet.exe"
+SET NuGet="..\packages\NuGet.CommandLine.2.8.5\tools\NuGet.exe"
 %NuGet% pack ".\SnkLib.App.CookieGetter.nuspec" -OutputDirectory "..\Publish" -Build -Properties Configuration=Release
 %NuGet% pack ".\SnkLib.App.CookieGetter.Forms.nuspec" -OutputDirectory "..\Publish" -Build -Properties Configuration=Release
